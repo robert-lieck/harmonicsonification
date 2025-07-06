674badaa13c6e57f176c020c10f669c664f4f402
Robert Lieck <robert.lieck@durham.ac.uk>
2025-07-06 04:24:11 +0100

    version 0.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="harmonicsonification",
-    version="0.0.1",
+    version="0.1.0",
     author="Robert Lieck",
     author_email="robert.lieck@durham.ac.uk",
     description="Sonify data using harmonic overtones ",
